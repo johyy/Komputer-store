213492c6b98fd7ea922d75cbf019a42b046b0f66
Jonna Hyypiä <jonna.hyypia@fi.experis.com>
2022-09-09 19:19:34 +0300

    Add all functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const laptopsElement = document.getElementById("laptops");
-const balanceElement = document.getElementById("balance")
+const balanceElement = document.getElementById("balance");
+const loanElement = document.getElementById("loan");
 const loanButtonElement = document.getElementById("getloan");
 const salaryElement = document.getElementById("salary")
 const bankButtonElement = document.getElementById("tobank");
@@ -10,11 +11,16 @@ const nameElement = document.getElementById("name");
 const infoElement = document.getElementById("info");
 const priceElement = document.getElementById("price");
 const buyButtonElement = document.getElementById("buy");
+const payloanElement = document.getElementById("payloan");
 
 let laptops = [];
 let totalSalary = 0;
 let totalLoan = 0;
 let total = 0;
+let loan = false;
+let repayButtonElement = null;
+let price = 0;
+let laptop = "";
 
 fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
     .then(response => response.json())
@@ -28,6 +34,8 @@ const addLaptopsToSelect = (laptops) => {
     pictureElement.src = "https://noroff-komputer-store-api.herokuapp.com/"+laptops[0].image;
     nameElement.innerText = laptops[0].title;
     infoElement.innerText = laptops[0].description;
+    price = laptops[0].price;
+    laptop = laptops[0].title;
 }
 
 const addLapTopToSelect = (laptop) => {
@@ -44,6 +52,7 @@ const handleLaptopSelectionChange = e => {
     priceElement.innerText = selectedLaptop.price;
     nameElement.innerText = selectedLaptop.title;
     infoElement.innerText = selectedLaptop.description;
+    price = selectedLaptop.price;
 }
 
 const handleWork = () => {
@@ -51,20 +60,89 @@ const handleWork = () => {
     salaryElement.innerText = totalSalary;
 }
 
+const handleGetLoan = () => {
+    const wantedLoan = prompt("Please enter the amount of money you wish to loan: ")
+    if (wantedLoan <= (total*2) && loan == false && wantedLoan > 0) {
+        totalLoan = wantedLoan;
+        total += parseInt(wantedLoan);
+        loanElement.innerHTML = totalLoan;
+        loan = true;
+        repayButtonElement = document.createElement("button");
+        repayButtonElement.innerText = "Repay Loan";
+        repayButtonElement.id = "repay";
+        payloanElement.appendChild(repayButtonElement)
+        repayButtonElement.addEventListener("click", handleRepayLoan);
+    } else if (loan == true) {
+        alert("You already have a loan.")      
+    } else {
+        alert("You can only get loan up to " + (total*2) + " euros.")
+    }
+
+    balanceElement.innerText = total;
+    loanElement.innerHTML = totalLoan;
+}
+
 const handleBank = () => {
-    total += totalSalary;
+    if (loan == false) {
+        total += totalSalary;
+    } else if ((totalSalary * 0.1) <= totalLoan) {
+        totalLoan -= totalSalary * 0.1;
+        total += totalSalary * 0.9;
+
+        if (totalLoan == 0) {
+            loan = false;
+            payloanElement.removeChild(repayButtonElement);
+        }
+    } else {
+        let residue = (totalSalary * 0.1) - totalLoan;
+        totalLoan = 0;
+        total += (totalSalary * 0.9) + residue;
+        loan = false;
+        payloanElement.removeChild(repayButtonElement);
+    }
+
     totalSalary = 0;
     salaryElement.innerText = totalSalary;
     balanceElement.innerText = total;
+    loanElement.innerText = totalLoan;
+}
 
+const handleRepayLoan = () => {
+    if (totalSalary >= totalLoan) {
+        let residue = (totalSalary-totalLoan);
+        totalLoan = 0;
+        loan = false;
+        payloanElement.removeChild(repayButtonElement);
+        total += residue;
+    } else {
+        totalLoan -= totalSalary;
+    }
+    
+    totalSalary = 0;
+    salaryElement.innerText = totalSalary;
+    balanceElement.innerText = total;
+    loanElement.innerHTML = totalLoan;
+}
+
+const handleBuyLaptop = () => {
+    if (price > total) {
+        alert("You cannot afford this laptop. This laptop costs " + price + " euros, but you only have " + total + " euros.")
+    } else {
+        alert("You are now the owner of a new laptop called " + laptop + ".")
+        total -= price;
+        balanceElement.innerText = total;
+    }
 }
 
 const handleAll = () => {
     salaryElement.innerText = totalSalary;
     balanceElement.innerText = total;
+    loanElement.innerText = totalLoan;
 }
 
 handleAll();
 laptopsElement.addEventListener("change", handleLaptopSelectionChange)
 workButtonElement.addEventListener("click", handleWork);
 bankButtonElement.addEventListener("click", handleBank);
+loanButtonElement.addEventListener("click", handleGetLoan);
+buyButtonElement.addEventListener("click", handleBuyLaptop);
